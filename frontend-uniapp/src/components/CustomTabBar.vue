290ef074a7fc7ff29b4118b590c6dee6788a8b9b
Cowork 3P <cowork-3p@localhost>
2026-07-15 12:57:10 +0800

    fix: use original PNGs as CSS masks for active tab icons to maintain visual consistency

--- frontend-uniapp/src/components/CustomTabBar.vue
+++ frontend-uniapp/src/components/CustomTabBar.vue
@@ -15,13 +15,13 @@
             :src="item.iconPath" 
             mode="aspectFit"
           />
-          <!-- Active SVG as Data URI for WeChat compatibility -->
-          <image 
+          <!-- Use CSS mask with original active PNGs to apply theme color -->
+          <view 
             v-else
-            class="tab-icon active-icon" 
-            :src="getActiveSvg(item.svg, currentTheme.primary)" 
-            mode="aspectFit"
-          />
+            class="tab-icon active-icon mask-icon" 
+            :class="item.maskClass"
+            :style="{ backgroundColor: currentTheme.primary }"
+          ></view>
         </view>
         <text 
           class="tab-text" 
@@ -41,39 +41,30 @@ const props = defineProps<{
   current: number
 }>();
 
-const getActiveSvg = (svgRaw: string, color: string) => {
-  const coloredSvg = svgRaw.replace(/currentColor/g, color);
-  return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(coloredSvg)}`;
-};
-
 const list = [
   {
     pagePath: '/pages/index/index',
     text: '日记',
     iconPath: '/static/tabs/diary.png',
-    // Book icon
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="100%" height="100%"><path d="M21 4H7a2 2 0 00-2 2v12a2 2 0 002 2h14a1 1 0 001-1V5a1 1 0 00-1-1zm-1 13H7a1 1 0 010-2h13v2zm0-4H7V6h13v7z"/><path d="M3 19V6a1 1 0 00-2 0v13a3 3 0 003 3h16a1 1 0 000-2H4a1 1 0 01-1-1z"/></svg>`
+    maskClass: 'mask-diary'
   },
   {
     pagePath: '/pages/analysis/analysis',
     text: '洞察',
     iconPath: '/static/tabs/analysis.png',
-    // Bulb icon
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="100%" height="100%"><path d="M12 2C8.13 2 5 5.13 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.87-3.13-7-7-7zm2.5 11.7l-.5.35V16h-4v-1.95l-.5-.35C8.07 12.68 7 10.94 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.94-1.07 3.68-2.5 4.7z"/><path d="M9 19h6v2H9z"/></svg>`
+    maskClass: 'mask-analysis'
   },
   {
     pagePath: '/pages/chat/chat',
     text: '陪伴',
     iconPath: '/static/tabs/chat.png',
-    // Chat icon
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="100%" height="100%"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17L4 17.17V4h16v12z"/><path d="M7 9h10v2H7zm0-3h10v2H7zm0 6h7v2H7z"/></svg>`
+    maskClass: 'mask-chat'
   },
   {
     pagePath: '/pages/profile/profile',
     text: '我的',
     iconPath: '/static/tabs/profile.png',
-    // User icon
-    svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="100%" height="100%"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>`
+    maskClass: 'mask-profile'
   }
 ];
 
@@ -135,6 +126,35 @@ const switchTab = (url: string, index: number) => {
 .tab-icon {
   width: 100%;
   height: 100%;
+}
+
+.mask-icon {
+  -webkit-mask-size: contain;
+  mask-size: contain;
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  -webkit-mask-position: center;
+  mask-position: center;
+}
+
+.mask-diary {
+  -webkit-mask-image: url('/static/tabs/diary-active.png');
+  mask-image: url('/static/tabs/diary-active.png');
+}
+
+.mask-analysis {
+  -webkit-mask-image: url('/static/tabs/analysis-active.png');
+  mask-image: url('/static/tabs/analysis-active.png');
+}
+
+.mask-chat {
+  -webkit-mask-image: url('/static/tabs/chat-active.png');
+  mask-image: url('/static/tabs/chat-active.png');
+}
+
+.mask-profile {
+  -webkit-mask-image: url('/static/tabs/profile-active.png');
+  mask-image: url('/static/tabs/profile-active.png');
 }
 
 .active-icon {
